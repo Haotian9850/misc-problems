038f8f0428d6c12926aee8b936f17003c87af67c
haotian <hl7gr@virginia.edu>
2018-11-20 23:03:04 -0500

    word break II

--- src/WordBreakII.java
+++ src/WordBreakII.java
@@ -1,20 +1,46 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class WordBreakII {
     /*
     * Word break II: return all possible breaking combinations
     * */
 
-    public static List<String> split(Set<String> dict, String sentence){
-        //DFS approach
-        List<String> result = new ArrayList<>();
-        return result;
+    public static List<String> findAll(String s, Set<String> dict){
+        //find all possible combinations
+        //DP with mem approach
+        //used HashMap<String, List<String>> as cache for better runtime
+        return DFS(s, dict, new HashMap<>());
     }
 
-    private void DFS(List<String> result, List<String> temp, Set<String> dict, String sentence){
-        //return condition
+    private static List<String> DFS(String sentence, Set<String> dict, HashMap<String, List<String>> cache){
+        if(cache.containsKey(sentence)){
+            return cache.get(sentence);
+        }
+        List<String> tempResult = new ArrayList<>();
+        for(String word : dict){
+            if(sentence.startsWith(word)){
+                String nextSen = sentence.substring(word.length());
+                //check return condition here!
+                if(nextSen.length() == 0){
+                    tempResult.add(word);
+                }else{
+                    List<String> resultList = DFS(nextSen, dict, cache);
+                    for(String i : resultList){
+                        tempResult.add(word + " " + i);
+                    }
+                }
+            }
+        }
+        //update mem
+        cache.put(sentence, tempResult);
+        return tempResult;
+    }
 
+    public static void main(String[] args){
+        Set<String> dict = new HashSet<>(Arrays.asList("cats", "cat", "and", "sand", "dog"));
+        List<String> result = findAll("catsanddog", dict);
+        for(String s : result){
+            System.out.print(s);
+        }
     }
 }
