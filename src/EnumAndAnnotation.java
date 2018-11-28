a397068d459db26b8b3fa82c0457363f3819d1be
haotian <hl7gr@virginia.edu>
2018-11-27 19:49:13 -0500

    enums

--- src/EnumAndAnnotation.java
+++ src/EnumAndAnnotation.java
@@ -1,2 +1,29 @@
 public class EnumAndAnnotation {
+    /*
+    * An enum type is a special data type that enables for a variable to be a set of predefined constants. The variable
+    * must be equal to one of the values that have been predefined for it. Common examples include compass directions
+    * (values of NORTH, SOUTH, EAST, and WEST) and the days of the week.
+    * Enums and annotations became available since Java 5
+    * */
+
+    public enum DaysInAWeek{
+        MONDAY,
+        TUESDAY,
+        WENDESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
+
+    public static boolean isWeekday(DaysInAWeek day){
+        return day != DaysInAWeek.SATURDAY && day != DaysInAWeek.SUNDAY;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println(isWeekday(DaysInAWeek.MONDAY));
+    }
+
+
 }
