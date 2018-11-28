9da2ecfb3e0468b635c4e5e591f8f73950ff67ea
haotian <hl7gr@virginia.edu>
2018-11-27 19:41:45 -0500

    enum and annotations

--- src/Clone.java
+++ src/Clone.java
@@ -2,10 +2,12 @@ public class Clone {
     public static void main(String[] args){
         int[] testArray = {1, 2, 3};
         System.out.println("testArray mem address: " + testArray);
+        System.out.println("testArray default toString" + testArray.toString());
         int[] copyArray = testArray.clone();
         System.out.println("copyArray mem address: " + copyArray);
         for(Integer i : copyArray){
             System.out.print(i);
         }
+
     }
 }
