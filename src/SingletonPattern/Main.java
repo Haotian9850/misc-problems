a3a3971c2befb744ec1e6b3ced6b0231aa7fcc9a
Haotian9850 <haotian9850@gmail.com>
2018-12-16 16:05:25 -0500

    fixed singleton pattern code

--- src/SingletonPattern/Main.java
+++ src/SingletonPattern/Main.java
@@ -3,9 +3,10 @@ package SingletonPattern;
 public class Main {
 
     public static void main(String[] args){
-        SingleObject demoObject = SingleObject.getInstance();
 
+        SingleObject demoObject = SingleObject.getInstance();
         System.out.println(demoObject.toString());
+    
     }
 
 }
--- src/SingletonPattern/SingleObject.java
+++ src/SingletonPattern/SingleObject.java
@@ -6,6 +6,12 @@ public class SingleObject {
     * double-checked locking for thread safety
     * */
 
+    /*
+    * 适用场景：一个被全局使用的类被频繁的创建和销毁
+    * 优点：内存里只有一个类，减少了内存的使用；避免了对资源的多重占用
+    * 缺点：没有接口，不能继承
+    * */
+
     private volatile static SingleObject singleton;
 
     //private constructor
@@ -15,7 +21,7 @@ public class SingleObject {
 
     public static SingleObject getInstance(){
         if(singleton == null){
-            synchronized (SingleObject.class){
+            synchronized (SingleObject.class){  //multi-thread safety
                 if(singleton == null){
                     //加载对象
                     singleton = new SingleObject();
