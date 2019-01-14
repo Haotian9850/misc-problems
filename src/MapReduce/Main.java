0562f0e4426d2bf483c56d1c2a9022024ad7d8cd
Haotian Liu <haotian@pop-os.localdomain>
2019-01-14 13:45:20 -0500

    mapper and reducer entity

--- src/MapReduce/Main.java
+++ src/MapReduce/Main.java
@@ -2,8 +2,12 @@ package MapReduce;
 
 public class Main {
 
+    /*
+    * Simple demo for mapper and reducers. Not a Hadoop job!
+    * */
+
     public static void main(String[] args){
-        
+
     }
 
 }
